3db6f67fdf017b392b73875c740445906c1a63c9
axemonk <axemonk667@gmail.com>
2020-11-13 22:10:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 include "header.php"; 
 ?>
 
-<article class="main-container">
-	
-	<div class="container">
+	<div class="container text-center">
 		<div class="row">
-			<div class="col-md-12 text-center">
+			<div class="col-md-12">
 				<h1> About us </h1> 
 				<p> Auctor varius interdum quam pellentesque nam duis etiam neque, mauris malesuada tincidunt primis augue montes faucibus, posuere porttitor libero ullamcorper ante aenean aliquam. Torquent nisl auctor cras varius nam faucibus posuere nec, habitasse mollis congue vel primis risus pharetra, semper lobortis dis fringilla massa integer leo. Etiam quis hendrerit ad risus dui faucibus dictum quam maecenas, vivamus himenaeos natoque ac libero fermentum interdum egestas magna velit, nunc inceptos dis pharetra fames sagittis pulvinar eleifend.
-				<br /> Magna dignissim nunc cursus eleifend tortor egestas feugiat blandit, eros lacinia nisl imperdiet nibh donec est duis ut, penatibus non sagittis senectus risus vehicula cubilia. Bibendum eros velit cursus mattis faucibus erat, habitant massa laoreet tempus leo orci quis, metus diam enim iaculis pharetra. Convallis mi sagittis curabitur platea sociis vitae sapien dignissim, risus pretium in porttitor massa commodo.
+				Magna dignissim nunc cursus eleifend tortor egestas feugiat blandit, eros lacinia nisl imperdiet nibh donec est duis ut, penatibus non sagittis senectus risus vehicula cubilia. Bibendum eros velit cursus mattis faucibus erat, habitant massa laoreet tempus leo orci quis, metus diam enim iaculis pharetra. Convallis mi sagittis curabitur platea sociis vitae sapien dignissim, risus pretium in porttitor massa commodo.
 			 </p>
 			</div>
 		</div>

2db76408bc6cfa64ce5089f81044c3eefb51d668
Tzivia W <txl163630@utdallas.edu>
2020-11-19 14:41:12 -0600

    Session and display data is active on some pages

--- class.user.php
+++ class.user.php
@@ -1,30 +1,58 @@
 <?php
-    class user{
-        static function getPets($username, $con){
-            $sql = "select pp_id, petname from petprofiles pp
-            where pp.pp_username = '$username'";
+class user{
 
-            $result = $con->query($sql);
-            $pets = [];
-            while($row = $result->fetch_assoc()) {
-                $pets[] = $row;
-            };
-            return $pets;
-        }
+    static function getPets($username, $con){
+        $sql = "select pp_id, petname from petprofiles pp
+        where pp.pp_username = '$username'";
 
-        static function getAvailability($binarydays, $con){
-            $arr = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-            
-            $search = $binarydays;
-            $search_arr = str_split($search);
-            $result = array();
-            foreach($search_arr as $key => $value){
-                if($value == 1){
-                    $result[] = $arr[$key];
-                }
-            }
-            echo implode(", ", $result);
-        }
+        $result = $con->query($sql);
+        $pets = [];
+        while($row = $result->fetch_assoc()) {
+            $pets[] = $row;
+        };
+        return $pets;
     }
 
-?>
+    static function getAvailability($binarydays, $con){
+        $arr = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        
+        $search = $binarydays;
+        $search_arr = str_split($search);
+        $result = array();
+        foreach($search_arr as $key => $value){
+            if($value == 1){
+                $result[] = $arr[$key];
+            }
+        }
+        echo implode(", ", $result);
+    }
+
+    function getUserDetails($username, $con){
+        $sql = "select * from users where username='$username'";
+
+        $result = $con->query($sql);
+        $row = $result->fetch_assoc();
+
+        return $row;
+    }
+
+    function getSitterDetails($username, $con){
+        $sql = "select * from sitterprofiles where sp_username ='$username'";
+        
+        $result = $con->query($sql);
+        $row = $result->fetch_assoc();
+        
+        return $row;
+    } 
+    function getPetDetails($username, $con){
+        $sql = "select * from petprofiles where pp_username ='$username'";
+        
+        $result = $con->query($sql);
+        $row = $result->fetch_assoc();
+        
+        return $row;
+    } 
+
+
+    }
+    

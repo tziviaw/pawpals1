134258f4d19b346e4fc3429c81062a024ae132be
axemonk <axemonk667@gmail.com>
2020-11-19 16:55:04 -0600

    Update class.petlisting.php

--- class.petlisting.php
+++ class.petlisting.php
@@ -1,39 +1,39 @@
 <?php
-class petlisting{
-    static public function getListings($con){
-        $sql = "select pl_id, pp_id, pp_img as 'img', petname, pettype, breed, size, pl_description as 'desc', contact,
-        pl_neededfrom, pl_neededto
-        from petlistings, petprofiles, users 
-        where pp_id = pl_pp_id 
-        and username = pp_username
-        order by pl_datecreated desc;";
+	class petlisting{
+			static public function getListings($con){
+					$sql = "select pl_id, pp_id, pp_img as 'img', petname, pettype, breed, size, pl_description as 'desc', contact,
+					pl_neededfrom, pl_neededto
+					from petlistings, petprofiles, users 
+					where pp_id = pl_pp_id 
+					and username = pp_username
+					order by pl_datecreated desc;";
 
-        $result = $con->query($sql);
-        return $result;
-    }
+					$result = $con->query($sql) or die($con->error);
+					return $result;
+			}
 
-    static function createListing($description, $username, $petid, $dateneededfrom, $dateneededto, $con){
-        $sql = "insert into petlistings (pl_username, pl_pp_id, pl_description, pl_neededfrom, 
-                pl_neededto) 
-                VALUES ('$username', $petid, '$description', '$dateneededfrom', '$dateneededto')";
-               
-        echo $sql;
-        $result = $con->query($sql);
-        return $result;
-    }
+			static function createListing($description, $username, $petid, $dateneededfrom, $dateneededto, $con){
+					$sql = "insert into petlistings (pl_username, pl_pp_id, pl_description, pl_neededfrom, 
+									pl_neededto) 
+									VALUES ('$username', $petid, '$description', '$dateneededfrom', '$dateneededto')";
+								 
+					echo $sql;
+					$result = $con->query($sql) or die($con->error);
+					return $result;
+			}
 
-    static function filterListing($search, $con){
-        $sql = "select pl_id, pp_id, pp_img as 'img', petname, pettype, breed, size, pl_description as 'desc', contact,
-        pl_neededfrom, pl_neededto
-        from petlistings, petprofiles, users 
-        where (pp_id = pl_pp_id 
-        and username = pp_username)
-        and (pettype like '$search' or breed like '$search' or size like '$search' or pl_description like '$search')
-        order by pl_datecreated desc;";
+			static function filterListing($search, $con){
+					$sql = "select pl_id, pp_id, pp_img as 'img', petname, pettype, breed, size, pl_description as 'desc', contact,
+					pl_neededfrom, pl_neededto
+					from petlistings, petprofiles, users 
+					where (pp_id = pl_pp_id 
+					and username = pp_username)
+					and (pettype like '$search' or breed like '$search' or size like '$search' or pl_description like '$search')
+					order by pl_datecreated desc;";
 
-        $result = $con->query($sql);
-        return $result;
-    }
+					$result = $con->query($sql) or die($con->error);
+					return $result;
+			}
 
-}
+	}
 ?>
